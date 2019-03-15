f651e67e97d70faee3d02aa70a74f9bd6083efba
Christopher <christopher.iliyas@andela.com>
2019-03-15 12:05:20 +0100

    remove files not needed

--- dummy/middleware/adminvalidator.js
+++
@@ -1,8 +0,0 @@
-import { adminRole } from '../models/usermodel';
-
-export default (req, res, next) => {
-  if (adminRole.length === 0) {
-    return res.status(403).send({ success: false, message: 'You are not permitted to access this page.' });
-  }
-  next();
-};
--- dummy/middleware/loggedinvalidator.js
+++
@@ -1,8 +0,0 @@
-import { loggedIn } from '../models/usermodel';
-
-export default (req, res, next) => {
-  if (loggedIn.length === 0) {
-    return res.status(401).send({ success: false, message: 'Please log in to your account.' });
-  }
-  next();
-};
